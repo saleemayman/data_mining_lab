19871d5946b452ca583800549b2a71e2aa0b1353
LehenDev <LehenDev@gmail.com>
2014-10-31 13:45:20 +0100

    Corrections
    Archived the functions previously uploaded because they were of course
    slow as hell. Corrected also a mistake in the data file.
    New data extractor. Wow no cells. Much Swag. Matrices.

--- SME_script.m
+++
@@ -1,8 +0,0 @@
-%SIMILARITY MATRIX EXPLOITATION Script
-%
-%In this script, the target is to use the similarity measure that we
-%defined the first week, aka string compare, in order to find clusters in
-%the data. As noticed, the data is apparently already structured. Let's see
-%how.
-
-
